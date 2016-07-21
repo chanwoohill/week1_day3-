86b987e1bf3dde3c4dfb962346b51321f20df3ee
Chanwoo Kim <chanwoohill@gmail.com>
2016-07-21 08:47:43 +0000

    add debug exercises

---
+++ debug01.rb
@@ -0,0 +1,7 @@
+list = {'yvr' => 'Vancouver'}, {'yba' => 'Banff'}, {'yyz' => 'Toronto'}, {'yxx' => 'Abbotsford'}, {'ybw' => 'Calgary'}
+
+# Why is it returning nil instead of first element of the list above
+# list is hash, not an array.
+
+p list.first
+
